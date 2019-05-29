437c7ea53f4c40077d938baa970b1edda1073ccc
snoestro <snoestro@users.noreply.github.com>
2019-05-29 10:27:45 +0900

    0529_1

--- Poing/src/poing/rest/RestDetailDAO.java
+++ Poing/src/poing/rest/RestDetailDAO.java
@@ -39,7 +39,7 @@ public class RestDetailDAO {
 				dto.setRest_view_cnt(rs.getInt("rest_view_cnt"));
 				dto.setRest_starpoint(rs.getDouble("rest_starpoint"));
 				dto.setRest_loc(rs.getString("rest_loc"));
-				dto.setRest_tic_code(rs.getInt("rest_tic_code"));
+				dto.setRest_tic_code(rs.getInt("p_code"));
 				dto.setRest_line_exp(rs.getString("rest_line_exp"));
 				dto.setRest_alchol(rs.getString("rest_alchol"));
 				dto.setRest_parking_yn(rs.getString("rest_parking_yn"));

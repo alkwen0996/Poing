79841ab53a95bfd38f114abaa9a1ce4dd1116b8a
kouzie <kgy1996@naver.com>
2019-06-14 12:48:24 +0900

    관리자 끝

--- Poing/src/admin/bannerimage/add/service/AddBannerImageService.java
+++ Poing/src/admin/bannerimage/add/service/AddBannerImageService.java
@@ -17,4 +17,5 @@ public class AddBannerImageService {
 		conn.close();
 		return result;
 	}
+	
 }
--- Poing/src/admin/productbanner/modify/service/ModifyProductBannerService.java
+++ Poing/src/admin/productbanner/modify/service/ModifyProductBannerService.java
@@ -6,14 +6,24 @@ import java.sql.SQLException;
 import com.util.ConnectionProvider;
 
 import admin.bannerimage.BannerImageDAO;
+import admin.bannerimage.ProductBannerDAO;
+import admin.bannerimage.ProductBannerDTO;
 
 public class ModifyProductBannerService {
-	BannerImageDAO bannerImageDAO = new BannerImageDAO();
-	public boolean addBannerImageList(String imagePath) throws SQLException {
+	ProductBannerDAO productBannerDAO = new ProductBannerDAO();
+	public ProductBannerDTO selectProductBanner(int pb_seq) throws SQLException {
+		ProductBannerDTO pbDTO = null;
+		Connection conn = null;
+		conn = ConnectionProvider.getConnection();
+		pbDTO = productBannerDAO.selectProductBanner(conn, pb_seq);
+		conn.close();
+		return pbDTO;
+	}
+	public boolean modifyProductBanner(int pb_seq, String pb_sale, String pb_title, String pb_descript, String pb_link) throws SQLException {
 		boolean result = false;
 		Connection conn = null;
 		conn = ConnectionProvider.getConnection();
-		result = bannerImageDAO.insertBannerImage(conn, imagePath);
+		result = productBannerDAO.updateProductBannerInfo(conn,pb_seq, pb_sale, pb_title, pb_descript, pb_link);
 		conn.close();
 		return result;
 	}
